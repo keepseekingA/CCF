a8a641ebd40e8e98ed9d275c0aa3f842b784bf2f
yi <442789045@qq.com>
2023-12-21 07:15:59 +0800

    Update 20231202标准题解

--- CSP/CCF202312/20231202-标准题解.py
+++ CSP/CCF202312/20231202-标准题解.py
@@ -1,24 +1,16 @@
-def get_primes(num):     # 求n以内的所有素数
-    prime_li = [True] * (num + 1)   # i * j = num
-    for i in range(2, num//2 +1):   # i
-        for j in range(2, num//i +1):   # j
-            prime_li[j * i] = False
-    return prime_li
-
-
-def decompose(n):
+def decompose(num):   # 将整数num用因子形式表示 (因子，幂)
     ans = []
     i = 2
-    while i * i <= n:
+    while i * i <= num:   # 检查2-sqrt(n)
         tmp = 0
-        while n % i == 0:
+        while num % i == 0:   # 素数筛选算法, 筛掉i的倍数，每筛一次，i的幂次+1
             tmp += 1
-            n //= i
+            num //= i
         if tmp > 0:
             ans.append((i, tmp))
         i += 1
-    if n > 1:
-        ans.append((n, 1))
+    if num > 1:   # 大于sqrt(n)的素因子最多只有1个
+        ans.append((num, 1))
     return ans
 
 
