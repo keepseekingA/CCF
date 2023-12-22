10caedec65c6803d4d5701fbee8decdd2a8fc701
yi <442789045@qq.com>
2023-12-22 19:52:48 +0800

    Update 为20230903部分分题解添加注释

--- CSP/CCF202309/20230903-部分分.py
+++ CSP/CCF202309/20230903-部分分.py
@@ -1,8 +1,8 @@
-def d_add(v1, v2, x):
+def d_add(v1, v2, x):   # +法求导
     return 1 if v1 == x or v2 == x else 0
 
 
-def d_sub(v1, v2, x):
+def d_sub(v1, v2, x):   # -法求导
     if v1 == x:
         return 1
     elif v2 == x:
@@ -11,7 +11,7 @@ def d_sub(v1, v2, x):
         return 0
 
 
-def d_mas(v1, v2, x_value):
+def d_mas(v1, v2, x_value):     # *法求导
     x = "x" + str(x_value[0])
     if v1 == x and v2 != x:
         return v2
@@ -33,27 +33,27 @@ def _get(rpn, x):      # 仅利用栈对逆波兰式解析,并直接求导
         else:
             b = stack.pop()
             a = stack.pop()
-            tmp = "(" + a + " " + item + " " + b + ")"
+            tmp = "(" + a + " " + item + " " + b + ")"      # 组成算式
             stack.append(tmp)
-    print(eval(stack[0]) % (10**9 +7))
+    print(eval(stack[0]) % (10**9 + 7))
 
 
-n, m = map(int, input().split())
-rpn = input().split()
+n, m = map(int, input().split())    # n,自变量的个数; m,要求解的偏导数的个数
+rpn = input().split()       # 逆波兰式
 rpn_len = len(rpn)
 for i in range(m):
-    dx_value = list(map(int, input().split()))
-    if rpn_len == 1:
+    dx_value = list(map(int, input().split()))      # 第1个整数是要求偏导数的自变量的编号；随后的整数是要求算的点的坐标
+    if rpn_len == 1:    # 仅有一项
         print(1 if rpn[0] == "x" + str(dx_value[0]) else 0)
-    elif rpn_len == 3:
+    elif rpn_len == 3:      # 仅有两项 运算符可能为 + - *
         if rpn[-1] == "+":
             r = d_add(rpn[0], rpn[1], "x" + str(dx_value[0]))
         elif rpn[-1] == "-":
             r = d_sub(rpn[0], rpn[1], "x" + str(dx_value[0]))
         else:
             r = d_mas(rpn[0], rpn[1], dx_value)
-        print(int(r) % (10 ** 9 + 7))
-    elif rpn_len > 3 and "*" not in rpn:
+        print(int(r) % (10 ** 9 + 7))   # 按格式输出，对应的偏导数对10**9 +7取模的结果
+    elif rpn_len > 3 and "*" not in rpn:    # 仅包含 + - 的情况
         _get(rpn, "x" + str(dx_value[0]))
     else:   # 乘法
         pass
